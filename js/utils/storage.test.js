8181ab358cdcf7b85970a27d8f6bf564f80e9182
tin-kri <113044805+tin-kri@users.noreply.github.com>
2025-04-11 14:57:56 +0200

    added test for getusername with jsdom

--- js/utils/storage.test.js
+++ js/utils/storage.test.js
@@ -1,26 +1,44 @@
-import { it, describe, expect, beforeEach } from "vitest";
-import { saveUser, getUsername } from "./storage";
+// import { it, describe, expect, beforeEach } from "vitest";
+// import { saveUser, getUsername } from "./storage";
+
+// describe("getUsername", () => {
+//   beforeEach(() => {
+//     const storage = {};
+
+//     this.localStorage = {
+//       setItem: (key, value) => (storage[key] = value),
+//       getItem: (key) => storage[key] || null,
+//     };
+//   });
+
+//   it("returns the name from the user object in storage", () => {
+//     const user = { name: "John Doe" };
+//     saveUser(user);
+
+//     const username = getUsername();
+//     expect(username).toBe("John Doe");
+//   });
+
+//   it("returns null when no user exists in storage", () => {
+//     const username = getUsername();
+//     expect(username).toBe();
+//   });
+// });
+
+import { expect, describe, it, beforeEach } from "vitest";
+import { getUsername, saveUser } from "./storage.js";
 
 describe("getUsername", () => {
   beforeEach(() => {
-    const storage = {};
-
-    this.localStorage = {
-      setItem: (key, value) => (storage[key] = value),
-      getItem: (key) => storage[key] || null,
-    };
+    localStorage.clear();
   });
 
-  it("returns the name from the user object in storage", () => {
-    const user = { name: "John Doe" };
-    saveUser(user);
-
-    const username = getUsername();
-    expect(username).toBe("John Doe");
+  it("Returns the name from the user object in local storage", () => {
+    saveUser({ name: "test-user" });
+    expect(getUsername()).toBe("test-user");
   });
 
-  it("returns null when no user exists in storage", () => {
-    const username = getUsername();
-    expect(username).toBe(null);
+  it("Returns null when no user exists in local storage", () => {
+    expect(getUsername()).toBe(null);
   });
 });
