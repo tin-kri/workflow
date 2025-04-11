5dd4ffb6ce84605423af6a147fce74e69d8bbba8
tin-kri <113044805+tin-kri@users.noreply.github.com>
2025-04-11 15:01:18 +0200

    deleted comments

--- js/utils/storage.test.js
+++ js/utils/storage.test.js
@@ -1,30 +1,3 @@
-// import { it, describe, expect, beforeEach } from "vitest";
-// import { saveUser, getUsername } from "./storage";
-
-// describe("getUsername", () => {
-//   beforeEach(() => {
-//     const storage = {};
-
-//     this.localStorage = {
-//       setItem: (key, value) => (storage[key] = value),
-//       getItem: (key) => storage[key] || null,
-//     };
-//   });
-
-//   it("returns the name from the user object in storage", () => {
-//     const user = { name: "John Doe" };
-//     saveUser(user);
-
-//     const username = getUsername();
-//     expect(username).toBe("John Doe");
-//   });
-
-//   it("returns null when no user exists in storage", () => {
-//     const username = getUsername();
-//     expect(username).toBe();
-//   });
-// });
-
 import { expect, describe, it, beforeEach } from "vitest";
 import { getUsername, saveUser } from "./storage.js";
 
